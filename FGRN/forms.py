76172e4797e7c5b44d99cdeadea95a9ef936cd56
Ludaze <luhanad@ymail.com>
2024-03-11 10:57:47 +0300

    GRN, FGRN and MR apps created

---
+++ FGRN/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+#from .models import GRN, GRNItem
+from .models import *
+
+
+class FGRNForm(forms.ModelForm):
+    
+    
+    class Meta:
+   
+        model = FGRN
+        fields = ['FGRN_no','date','recieved_from']
+   
+class FGRNItemForm(forms.ModelForm):
+    
+    item_name = forms.CharField(
+        widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Item Name'})
+    )
+    quantity = forms.IntegerField(
+        widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'quantity'})
+    )
+    remarks = forms.CharField(
+        required = False,
+        widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add Description'})
+    )
+    class Meta:
+   
+        model = FGRN_item
+        fields = ['item_name','quantity','remarks']
+       
---
+++ FGRN/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class FGRN(models.Model):
+    FGRN_no = models.TextField(primary_key= True)
+    recieved_from = models.TextField(blank=True, null= True)
+    date = models.DateField()
+
+class FGRN_item(models.Model):
+    FGRN_no = models.ForeignKey('FGRN', on_delete=models.CASCADE,db_column = 'FGRN_no')
+    item_id = models.AutoField(primary_key= True)
+    item_name = models.TextField(blank=True, null= True)
+    quantity = quantity = models.FloatField()
+    remarks = models.TextField(blank=True, null= True)
+
+class finished_goods(models.Model):
+    item_name = models.TextField(primary_key=True)
+    quantity = quantity = models.FloatField()
